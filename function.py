98c3676a13931fcb0a2a7b2a2c99234257de76f1
Tacher3000 <puankovnikita386@gmail.com>
2023-08-13 21:11:04 +0400

    com1

---
+++ function.py
@@ -0,0 +1,263 @@
+import requests
+import datetime
+import os
+import random
+import yadisk
+from vk_api import VkApi, VkUpload
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Для яндекс диска
+yadisk_token = os.environ['YADISK_TOKEN']
+y = yadisk.YaDisk(
+    token=yadisk_token)
+
+# для вк
+token = os.environ["TOKEN"]
+vk_session = VkApi(token=token)
+vk = vk_session.get_api()
+
+
+# возвращяет текст из тестового файла
+
+
+def open_txt(txt_name):
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(script_directory, 'data/txt', txt_name)
+    with open(data_file, 'r', encoding='utf-8') as file:
+        output = file.read()
+    return output
+
+# читает и возращает определнную строку из файла
+
+
+def open_txt_line(number, txt_name):
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(script_directory, 'data/txt', txt_name)
+    with open(data_file, 'r', encoding='utf-8') as file:
+        if number == 1:
+            output = file.readline()
+            print(output)
+            return output
+        lines = file.readlines()
+        output = lines[number]
+        return output
+
+# возращает сколько в файле строк
+
+
+def count_lines(txt_name):
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(script_directory, 'data/txt', txt_name)
+    with open(data_file, 'r', encoding='utf-8') as file:
+        line_count = 0
+        for line in file:
+            line_count += 1
+        return line_count
+
+
+# отправляет сообщениие
+def send_message(id, message):
+    vk.messages.send(
+        peer_id=id,
+        message=message,
+        random_id=get_random_id(),
+    )
+
+# отправляет файл
+
+
+def send_txt_file(id, message, file_path):
+
+    upload_url = vk.docs.getMessagesUploadServer(
+        type='doc', peer_id=id)['upload_url']
+
+    file = {'file': open('data/txt/' + file_path, 'rb')}
+    response = requests.post(upload_url, files=file)
+    result = response.json()
+
+    doc = vk.docs.save(file=result['file'],
+                       title=file_path)['doc']
+
+    vk.messages.send(peer_id=id,
+                     message=message,
+                     attachment=f"doc{doc['owner_id']}_{doc['id']}",
+                     random_id=get_random_id())
+
+# отправляет изображения
+
+
+def send_images(id, message, images_path):
+    upload_url = vk_session.method('photos.getMessagesUploadServer', {
+                                   'peer_id': id})['upload_url']
+    response = requests.post(
+        upload_url, files={'photo': open('data/images/' + images_path, 'rb')}).json()
+    photo_data = vk_session.method('photos.saveMessagesPhoto', {
+                                   'photo': response['photo'], 'server': response['server'], 'hash': response['hash']})[0]
+    attachment = f"photo{photo_data['owner_id']}_{photo_data['id']}_{photo_data['access_key']}"
+    vk.messages.send(peer_id=id,
+                     message=message,
+                     attachment=attachment,
+                     random_id=get_random_id())
+
+
+# история сообщений от пользователей
+def history_message(event, message):
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(script_directory, 'data/txt/history_message.txt')
+    file = open(data_file, 'a', encoding='utf-8')
+    file.write(
+        f"{event.datetime}\tпользователь {event.user_id} написал: {message}\n")
+    file.close()
+    return
+
+
+
+def marks(id):
+    return vk.messages.getLastActivity(user_id = id)
+
+
+# переделать прослушку
+# погода
+def water(id, longpoll):
+    api_key = os.environ['WATER_TOKEN']
+    base_url = "http://api.openweathermap.org/data/2.5/forecast?"
+
+    send_message(id, 'Введите название города: ')
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.user_id == id:
+            city_name = event.text
+            break
+
+    send_message(
+        id, 'Выберите прогноз погоды: 1 - сейчас, 2 - на сегодня, 3 - на 3 дня вперед: ')
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.user_id == id:
+            forecast_choice = event.text
+            break
+
+    complete_url = base_url + "appid=" + api_key + \
+        "&q=" + city_name + "&units=metric"
+
+    response = requests.get(complete_url)
+    data = response.json()
+
+    if data["cod"] != "404":
+        if forecast_choice == '1':
+            current_temp = data['list'][0]['main']['temp']
+            send_message(
+                id, (f"Текущая температура в городе {city_name} : {current_temp}°C"))
+            return
+        elif forecast_choice == '2':
+            today_temp = data['list'][0]['main']['temp']
+            send_message(
+                id, (f"Температура на сегодня в городе {city_name} : {today_temp}°C"))
+            return
+        elif forecast_choice == '3':
+            day1_temp = data['list'][0]['main']['temp']
+            day2_temp = data['list'][1]['main']['temp']
+            day3_temp = data['list'][2]['main']['temp']
+            send_message(
+                id, (f"Температура на 3 дня вперед в городе {city_name} : {day1_temp}°C, {day2_temp}°C, {day3_temp}°C"))
+            return
+        else:
+            send_message(id, "Неверный выбор прогноза погоды")
+            return
+    else:
+        send_message(id, "Город не найден")
+        return
+
+# отправляет дневник
+
+
+def diary():
+    # удаляем старый файл если он есть
+    try:
+        os.remove('data/txt/diary.txt')
+    except FileNotFoundError:
+        print('Файл для удаления не найден.')
+
+    # скачиваем новый с яндекс диска
+    y.download('/diary.txt', 'data/txt/diary.txt')
+
+    # отправляем файл каждому человеку из списка
+    for j in range(count_lines('trusted_people.txt')):
+        send_txt_file(
+            int(open_txt_line(j, 'trusted_people.txt')), 'Мой дневник, сообщение оптравлено автоматически(возможно это ошибка, не спеши читать. И вообще не читай если не хочешь. Читай только в том случае если тебе не все равно).', 'diary.txt')
+
+# отправляет дневник самому себе
+
+
+def me_diary(id, message):
+    # удаляем старый файл если он есть
+    try:
+        os.remove('data/txt/diary.txt')
+    except FileNotFoundError:
+        print('Файл для удаления не найден.')
+
+    # скачиваем новый с яндекс диска
+    y.download('/diary.txt', 'data/txt/diary.txt')
+
+    send_txt_file(id, message, 'diary.txt')
+
+# скачивает изображение с яндекс диска
+
+
+def download_images_yadisk():
+    # Получаем текущую дату
+    current_date = datetime.date.today()
+
+    # Получаем день недели (0 - понедельник, 1 - вторник, ..., 6 - воскресенье)
+    day_of_week = current_date.weekday()
+
+    path_to_day = {
+        0 : '/mems',
+        1 : '/Screenshots',
+        2 : '/minecraft screenshots',
+        3 : '/download',
+        4 : '/mems',
+        5 : '/Screenshots',
+        6 : '/minecraft screenshots',
+    }
+
+    # получаем путь от дня недели
+    if day_of_week in path_to_day:
+        path = path_to_day[day_of_week]
+    else:
+        print('день не найден')
+        path = '/mems'
+
+    photo_names = []
+
+    for item in y.listdir(path):
+        if item.type == 'file':
+            photo_names.append(item.name)
+
+    random_photo = random.choice(photo_names)
+    y.download(f'{path}/{random_photo}', 'data/images/' + random_photo)
+    return random_photo
+
+
+def add_diary(message):
+    # удаляем файл(если есть)
+    try:
+        os.remove('data/txt/diary.txt')
+    except FileNotFoundError:
+        print('Файл для удаления не найден.')
+
+    # скачиваем новый с яндекс диска
+    y.download('/diary.txt', 'data/txt/diary.txt')
+
+    # добавляем запись
+    with open('data/txt/diary.txt', 'a', encoding='utf-8') as file:
+        file.write(f'{message}\n\n')
+
+    # загружаем измененную версию на диск
+    y.upload('data/txt/diary.txt', '/diary.txt', overwrite=True)
---
+++ main.py
@@ -0,0 +1,202 @@
+import requests
+import time
+import os
+from vk_api import VkApi
+from vk_api.longpoll import VkLongPoll, VkEventType
+import function as f
+
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# через сколько дней попросят отметиться
+warning = 0.2
+
+# через сколько дней без отметок отправится дневник
+deadline = 3
+
+
+slovar = {'привет': 'Привет!',
+          'как дела': 'Хорошо, а у тебя?',
+          'как дела?': 'иди нахуй(без обид)',
+          'воткинск': 'Столица мира'}
+
+# чтобы не выключался каждую ночь + когда срабатывает исключение добавляется +1 и происходит проверка на колличество дней
+
+
+class MyVkLongPoll(VkLongPoll):
+    def listen(self):
+        while True:
+            try:
+                for event in self.check():
+                    yield event
+            except Exception as e:
+                print('error', e)
+                time.sleep(10)
+
+
+                current_timestamp = int(time.time())
+                timestamp = f.marks(240453492)
+                t = timestamp['time']
+
+                # проверка на колличество дней
+                if (current_timestamp - t > warning * 24 * 60 * 60) and (current_timestamp - t < deadline * 24 * 60 * 60):
+                    f.send_message(
+                        240453492, f'в последний раз ты был онлайн {warning} или больше дней назад. Пожалуйста, зайди в сеть')
+                    f.send_message(
+                        240453492, f'через столько дней:{(current_timestamp - t) / (24 * 60 * 60) } дневник будет отправлен')
+
+                if (current_timestamp - t > deadline * 24 * 60 * 60):
+                    f.send_message(
+                        240453492, f'ты не заходил больше {deadline} дней')
+                    f.diary()
+                    f.send_message(
+                        240453492, 'дневник отправлен')
+
+
+def main():
+    print('запустилось')
+    # token = f.open_txt('access_token.txt')
+    token = os.environ['TOKEN']
+    vk_session = VkApi(token=token)
+    vk = vk_session.get_api()
+    longpoll = MyVkLongPoll(vk_session)
+
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            k = 0
+            # message
+            message = event.text
+
+            # добавление в историю
+            f.history_message(event, message)
+
+            # преобразование слова в нижний регистр
+            message = message.lower()
+
+            # id
+            id = event.user_id
+
+            # проверяет есть ли человек который пишет в списке администрации
+            for i in range(f.count_lines('administrators.txt')):
+                if id == int(f.open_txt_line(i, 'administrators.txt')):
+                    if (message == '!отметиться' or message == '!очисть' or message == '!очистить'):
+                        f.check_in(id)
+                        k += 1
+                    elif (message == '!сколько осталось?' or message == '!остаток' or message == '!сколько'):
+                        f.send_message(id, f.passed())
+                        k += 1
+                    elif message == '!команды':
+                        f.send_message(id, f.open_txt(
+                            'administrators_comands.txt'))
+                        k += 1
+                    elif message == '!погода':
+                        f.water(id, longpoll)
+                        k += 1
+                    elif slovar.get(message) != None:
+                        f.send_message(id, slovar.get(message))
+                        k += 1
+                    elif message == '!отправь':
+                        f.send_message(
+                            id, 'Ты точно хочешь это сделать? [Y - да, N - нет]')
+                        # немного переделать!!!!!!
+                        for e in longpoll.listen():
+                            if e.type == VkEventType.MESSAGE_NEW and e.to_me:
+                                if e.text == 'Y':
+                                    f.diary()
+                                    break
+                                else:
+                                    f.send_message(id, 'Отправка отменена')
+                                    break
+                        k += 1
+                    elif message == '!рандом':
+                        random_image = f.download_images_yadisk()
+                        f.send_images(id, '', random_image)
+                        try:
+                            os.remove('data/images/' + random_image)
+                        except FileNotFoundError:
+                            print(
+                                'изображение для удаления не найдено(что странно).')
+                        k += 1
+                    elif message == '!прибавить':
+                        f.mark1()
+                        f.send_message(id, 'добавлен 1 день')
+                        k += 1
+                    elif message == '!посмотреть':
+                        f.me_diary(id, '')
+                        k += 1
+                    elif message == '!дополнить' or message == '!добавить':
+                        f.send_message(id, 'введите что хотите добавить')
+                        for e in longpoll.listen():
+                            if e.type == VkEventType.MESSAGE_NEW and e.to_me:
+                                f.add_diary(e.text)
+                                break
+                        f.send_message(id, 'сообщение добавлено')
+                        k += 1
+                    elif message == '!инфо':
+                        f.send_message(id, f.open_txt('info.txt'))
+                        k += 1
+
+            if k == 1:
+                continue
+
+            # проверка на людей из списка проверенных людей
+            for i in range(f.count_lines('trusted_people.txt')):
+                if id == int(f.open_txt_line(i, 'trusted_people.txt')):
+                    if (message == '!отправь'):
+                        f.diary()
+                        k += 1
+                    elif slovar.get(message) != None:
+                        f.send_message(id, slovar.get(message))
+                        k += 1
+                    elif message == '!команды':
+                        f.send_message(id, f.open_txt(
+                            'trusted_people_comands.txt'))
+                        k += 1
+                    elif message == '!погода':
+                        f.water(id, longpoll)
+                        k += 1
+                    elif message == '!рандом':
+                        random_image = f.download_images_yadisk()
+                        f.send_images(id, '', random_image)
+                        try:
+                            os.remove('data/images/' + random_image)
+                        except FileNotFoundError:
+                            print(
+                                'изображение для удаления не найдено(что странно).')
+                        k += 1
+                    elif message == '!инфо':
+                        f.send_message(id, f.open_txt('info.txt'))
+                        k += 1
+                    else:
+                        f.send_message(
+                            id, 'Ларочка, к сожалению, я всего лишь бот')
+                        k += 1
+                    
+
+            if k == 1:
+                continue
+
+            if message == '!команды':
+                f.send_message(id, f.open_txt('comands.txt'))
+            elif message == '!погода':
+                f.water(id, longpoll)
+            elif message == '!рандом':
+                random_image = f.download_images_yadisk()
+                f.send_images(id, '', random_image)
+                try:
+                    os.remove('data/images/' + random_image)
+                except FileNotFoundError:
+                    print(
+                        'изображение для удаления не найдено(что странно).')
+                k += 1
+            elif message == '!инфо':
+                        f.send_message(id, f.open_txt('info.txt'))
+                        k += 1
+
+
+if __name__ == '__main__':
+    main()
